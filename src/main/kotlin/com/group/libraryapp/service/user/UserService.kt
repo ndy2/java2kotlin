4b56e181a3395dcc79cd0bb8744b25ac8a3c30a1
ndy2 <emrdbs12@gmail.com>
2022-12-07 21:14:57 +0900

    lec17: Optional 제거

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -5,6 +5,8 @@ import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.util.fail
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,27 +16,27 @@ class UserService(
 ) {
 
     @Transactional
-    fun saveUser(request: UserCreateRequest){
+    fun saveUser(request: UserCreateRequest) {
 
         val newUser = User(request.name, request.age)
         userRepository.save(newUser)
     }
 
     @Transactional(readOnly = false)
-    fun getUsers(): List<UserResponse>{
+    fun getUsers(): List<UserResponse> {
         return userRepository.findAll()
             .map { UserResponse(it) }
     }
 
     @Transactional
-    fun updateUserName(request: UserUpdateRequest){
-        val user = userRepository.findById(request.id).orElseThrow { IllegalArgumentException() }
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findByIdOrNull(request.id) ?: fail()
         user.updateName(request.name)
     }
 
     @Transactional
     fun deleteUser(name: String) {
-        val user = userRepository.findByName(name).orElseThrow { IllegalArgumentException() }
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 }

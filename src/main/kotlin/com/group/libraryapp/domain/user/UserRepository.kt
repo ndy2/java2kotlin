cf608e787f9ffa1f0d91b2adf515944f09f20265
ndy2 <emrdbs12@gmail.com>
2022-12-08 10:32:40 +0900

    lec25: UserService.getUserLoanHistories

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,8 +1,18 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long>{
 
     fun findByName(name: String): User?
+
+    //@formatter:off
+    @Query(
+        "select u " +
+        "from User u " +
+        "left join u.userLoanHistories "
+    )
+    //@formatter:on
+    fun findAllWithUserLoanHistories() : List<User>
 }
---
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -0,0 +1,36 @@
+package com.group.libraryapp.dto.user.response
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
+
+data class UserLoanHistoryResponse(
+    val name: String, // username
+    val books: List<BookHistoryResponse>,
+) {
+
+    companion object {
+        fun of(user: User): UserLoanHistoryResponse {
+            return UserLoanHistoryResponse(
+                user.name,
+                user.userLoanHistories
+                    .map { BookHistoryResponse.of(it) }
+            )
+        }
+    }
+
+}
+
+data class BookHistoryResponse(
+    val name: String,   //bookname
+    val isReturn: Boolean,
+) {
+    companion object {
+        fun of(entity: UserLoanHistory): BookHistoryResponse {
+            return BookHistoryResponse(
+                entity.bookName,
+                entity.status == UserLoanStatus.RETURNED,
+            )
+        }
+    }
+}

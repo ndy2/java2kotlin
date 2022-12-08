886341a07d38d8e5955816980a2d459274ec49c4
ndy2 <emrdbs12@gmail.com>
2022-12-08 11:40:58 +0900

    lec32: 통계성 쿼리 추가

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -1,8 +1,18 @@
 package com.group.libraryapp.domain.user.loanhistory
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+
+    //@formatter:off
+    @Query(
+        "select count (h)" +
+        "from UserLoanHistory h " +
+        "where h.status = com.group.libraryapp.domain.user.loanhistory.UserLoanStatus.LOANED"
+    )
+    //@formatter:on
+    fun countLoanedBook(): Int
 }

4d520681a65a384af041631d4623dde5066bc91d
ndy2 <emrdbs12@gmail.com>
2022-12-08 11:40:58 +0900

    lec32: 통계성 쿼리 추가

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -1,8 +1,20 @@
 package com.group.libraryapp.domain.book
 
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface BookRepository : JpaRepository<Book, Long> {
 
-    fun findByName(bookName: String) : Book?
+    fun findByName(bookName: String): Book?
+
+    //@formatter:off
+    @Query(
+        "select new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, count(b)) " +
+        "from Book b " +
+        "group by b.type"
+    )
+    //@formatter:on
+    fun getBookStatistics(): List<BookStatResponse>
+
 }
--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -2,12 +2,14 @@ package com.group.libraryapp.service.book
 
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.book.BookType
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -40,6 +42,19 @@ class BookService(
     fun returnBook(request: BookReturnRequest) {
         val user = userRepository.findByName(request.userName) ?: fail()
         user.returnBook(request.bookName)
+    }
+
+    @Transactional(readOnly = true)
+    fun countLoanedBook(): Int {
+        return userLoanHistoryRepository.countLoanedBook()
+    }
+
+    fun getBookStatistics(): List<BookStatResponse> {
+        val results = bookRepository.getBookStatistics().toMutableList()
+        BookType.values()
+            .filterNot { type -> results.map { it.type }.contains(type) }
+            .forEach { results.add(BookStatResponse(it, 0L)) }
+        return results.toList()
     }
 
 }
